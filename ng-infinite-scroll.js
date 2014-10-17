ca0f696aebdf52edc10e4040f7999673cbc091c1
Andrei Shevel <ash.developer@yandex.ru>
2014-10-17 19:04:12 +0300

    added watching of filter update for prevent duplicates loading

--- ng-infinite-scroll.js
+++ ng-infinite-scroll.js
@@ -18,28 +18,30 @@ var ash = angular.module('ash', []).directive("ngInfiniteScroll", function ($tim
       $scope.offset = 0;
       $scope.inProcess = false;
       $scope.hasItems = true;
-
+      $scope.filterUpdateCount = 0;
+      
       if (!$scope.options.data.resource && !Array.isArray($scope.options.data)) {
         $scope.options.data = [$scope.options.data];
       }
       if ($scope.options.data.resource) {
         $scope.strategy = Resource;
         $scope.$watch('options.data.filter', function() {
-          console.log('filter');
+          $scope.filterUpdateCount++;
+          
           $scope.items = [];
           $scope.lastRemain = undefined;
           $scope.offset = 0;
           $scope.inProcess = false;
           $scope.hasItems = true;
-          $scope.strategy.addItems($scope);
+          $scope.strategy.addItems($scope, $scope.filterUpdateCount);
         }, true);
       } else {
         $scope.strategy = Data;
+        $scope.strategy.addItems($scope);
       }
-      $scope.strategy.addItems($scope);
 
       var checkRemainSpace = function(remain) {
-        if (remain < $scope.options.threshold && (!$scope.lastRemain || (remain - $scope.lastRemain) < 0) && $scope.hasItems && !$scope.inProcess) {
+        if (remain < $scope.options.threshold && (!$scope.lastRemain || (remain - $scope.lastRemain) < 0) && $scope.hasItems) {
           $scope.$apply(function() {
             $scope.strategy.addItems($scope);
           });
@@ -67,52 +69,60 @@ var ash = angular.module('ash', []).directive("ngInfiniteScroll", function ($tim
 ash.factory('Data', function() {
   return {
     addItems: function($scope) {
-      $scope.inProcess = true;
-
-      var from = $scope.offset * $scope.options.limit;
-      if (from < $scope.options.data.length) {
-        var to = from + $scope.options.limit;
-        to = to > $scope.options.data.length ? $scope.options.data.length : to;
-
-        for (var i = from; i < to; i++) {
-          $scope.items = $scope.items.concat($scope.options.data[i]);
+      if (!$scope.inProcess) {
+        $scope.inProcess = true;
+  
+        var from = $scope.offset * $scope.options.limit;
+        if (from < $scope.options.data.length) {
+          var to = from + $scope.options.limit;
+          to = to > $scope.options.data.length ? $scope.options.data.length : to;
+  
+          for (var i = from; i < to; i++) {
+            $scope.items = $scope.items.concat($scope.options.data[i]);
+          }
+  
+          $scope.offset++;
+        } else {
+          $scope.hasItems = false;
         }
-
-        $scope.offset++;
-      } else {
-        $scope.hasItems = false;
+  
+        $scope.inProcess = false;
       }
-
-      $scope.inProcess = false;
     }
   };
 });
 
 ash.factory('Resource', function() {
   return {
-    addItems: function($scope) {
-      $scope.inProcess = true;
-      var params = angular.extend(
-        { offset: $scope.offset * $scope.options.limit, limit: $scope.options.limit },
-        $scope.options.data.filter
-      );
-      
-      $scope.options.data.resource.query(
-        params,
-        function (data) {
-          if (data.length == 0) {
-            $scope.hasItems = false;
-          } else {
-            for (var i = 0; i < data.length; i++) {
-              $scope.items = $scope.items.concat(data[i]);
+    addItems: function($scope, currentFilterUpdateIteration) {
+      if (!currentFilterUpdateIteration) {
+        currentFilterUpdateIteration = $scope.filterUpdateCount;
+      }
+      if (!$scope.inProcess) {
+        $scope.inProcess = true;
+        var params = angular.extend(
+          { offset: $scope.offset * $scope.options.limit, limit: $scope.options.limit },
+          $scope.options.data.filter
+        );
+        
+        $scope.options.data.resource.query(
+          params,
+          function (data) {
+            if (data.length == 0) {
+              $scope.hasItems = false;
+            } else {
+              if (currentFilterUpdateIteration == $scope.filterUpdateCount) {
+                for (var i = 0; i < data.length; i++) {
+                  $scope.items = $scope.items.concat(data[i]);
+                }
+                $scope.offset++;
+              }
             }
-            $scope.offset++;
+  
+            $scope.inProcess = false;
           }
-
-          $scope.inProcess = false;
-        }
-      );
-
+        );
+      }
     }
   };
 });
